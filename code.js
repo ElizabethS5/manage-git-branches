be8f29b363de0607130720931685881068e30bbf
Elizabeth5 <escheidt5@gmail.com>
2021-09-15 03:17:21 -0400

    completed

---
+++ code.js
@@ -0,0 +1,13 @@
+const links = [...document.querySelectorAll("li a")];
+
+links.forEach((link) => {
+  link.addEventListener("click", click);
+});
+
+function click(e) {
+  links.forEach((link) => {
+    link.classList.remove("highlight");
+  });
+  let link = e.currentTarget;
+  link.classList.add("highlight");
+}
